22c3c6b8b86da038d205240566abd900451a5998
fareskhalil <fares.mohalhel@gmail.com>
2023-09-14 07:17:59 +0300

    finished initial design

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import { animate, timeline } from "motion";
+import gsap from "gsap";
+window.addEventListener("load", (e) => {
+  const kiryuTattooContainer = document.getElementById(
+    "kiryu-tattoo-container"
+  );
+  const kiryuTattoo = document.getElementById("kiryu-tattoo");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  const tl = new gsap.timeline();
+  // tl.from(kiryuTattooContainer, {
+  //   x: "150%",
+  //   duration: 1,
+  //   ease: "power2.Out",
+  //   // onComplete() {
+  //   //   timeline([[".cls-1", draw(1), { duration: 2 }]]);
+  //   // },
+  // }).to(
+  //   kiryuTattooContainer,
+  //   {
+  //     x: "-100%",
+  //     duration: 2,
+  //     ease: "power2.Out",
+  //   },
+  //   "<4"
+  // );
 
-setupCounter(document.querySelector('#counter'))
+  timeline([
+    [kiryuTattooContainer, { x: "100%" }, { duration: 0.0001 }],
+    [kiryuTattooContainer, { x: "0" }, { duration: 1 }],
+    // [
+    //   "path",
+    //   {
+    //     strokeDashoffset: 0,
+    //     visibility: "visible",
+    //     easing: "ease-out",
+    //   },
+    //   { duration: 3 },
+    // ],
+    [
+      kiryuTattooContainer,
+      { x: "-100%" },
+      { duration: 1, easing: "ease-in-out", delay: 0.5 },
+    ],
+  ]);
+
+  // end of load listener
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "kiryu-shirt": "#741920",
+        "kiryu-jacket": "#c5c8cb",
+        "kiryu-jacket-light": "#eaedf1",
+        "kiryu-shirt-light": "#9a212a",
+      },
+    },
+  },
+  plugins: [],
+};
